1a5be9a84586db7c38f1cde06e9b4c954e63b46d
Альбина Джорубова <deletawer@gmail.com>
2024-02-05 15:20:48 +0300

    display block main elements

--- js.js
+++ js.js
@@ -1,7 +1,11 @@
+const hiddenBlock = document.getElementsByClassName('hiddenBlock')[0];
+
 function todoAdd(point){
     if(event.key == 'Enter'){
-        if(point.value.trim()!== '' && point.value!== null){
-            console.log(point.value)
+        if(point.value.trim() !== '' && point.value !== null){
+            console.log(point.value);
+            hiddenBlock.classList.add('active');
+            point.value = ""
         }
     }
 }
