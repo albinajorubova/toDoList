8eb6b8f7bb26746c1a0bccffb0018e05400d0d92
Альбина Джорубова <deletawer@gmail.com>
2024-02-10 14:11:12 +0300

    add function displayMessages

--- js.js
+++ js.js
@@ -1,11 +1,37 @@
 const hiddenBlock = document.getElementsByClassName('hiddenBlock')[0];
+let toDoItem = document.querySelector('.mainList')
+let toDoList = []; 
 
 function todoAdd(point){
     if(event.key == 'Enter'){
         if(point.value.trim() !== '' && point.value !== null){
-            console.log(point.value);
+            let inputValue = point.value;
+            point.value = "";
             hiddenBlock.classList.add('active');
-            point.value = ""
+            document.getElementById("labelTxt").textContent = inputValue; 
+            let newToDo ={
+                toDoTxt: inputValue,
+                checked: false
+            }
+            toDoList.push(newToDo);
+            // console.log(toDoList)
+            displayMessages();
         }
     }
+}
+
+function displayMessages(){
+    let displayMessage = '';
+    toDoList.forEach(function(item, i){
+    displayMessage += `
+    <div class='list__item'>
+    <section class='leftItem'>
+      <input type='checkbox' name='checkInput' id='item__${i}'  />
+      <label for='item__${i}' id='labelTxt'>${item.toDoItem}</label>
+    </section>
+    <button class='delX'></button>
+  </div>
+    `;
+    toDoItem.innerHTML = displayMessage;
+    });
 }
