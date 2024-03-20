6675ff9c94e9eb74d7db61d274333c7f83491a54
albina <dzorubovaa@gmail.com>
2024-03-20 14:40:53 +0300

    fix comments 1203 add todoListContainer, changed func names, del console error, from let to const, optimized code

--- js.js
+++ js.js
@@ -1,128 +1,128 @@
 const hiddenBlock = document.querySelector('.tasksContainer');
 const allComplBtn = document.querySelector('.allComplBtn');
-let toDoItem = document.querySelector('.tasksList');
-let toDoList = []; 
-let btnAllCheck = document.querySelector('.allCompl__label');
-let deleteBtnItems = document.querySelectorAll('.taskItem__deleteBtn');
+const toDoItem = document.querySelector('.tasksList');
+const btnAllCheck = document.querySelector('.allCompl__label');
+const deleteBtnItems = document.querySelectorAll('.taskItem__deleteBtn');
 const tabsBtn = document.querySelectorAll('.tasksFilter__btn');
+const todoInput = document.querySelector(".todoInputBlock__input");
+const clearComplBtn = document.querySelector('.clearComplBtn');
+const spanLeft = document.querySelector('.tasksCount');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const toDoList = [];
+let countLeft;
 
-if(localStorage.getItem('todo')){  
-    toDoList = JSON.parse(localStorage.getItem("todo"));
+
+if (localStorage.getItem('todo')) {
+    const savedToDoList = JSON.parse(localStorage.getItem("todo"));
+    toDoList.push(...savedToDoList);
     hiddenBlock.classList.add('showBlock');
-    displayMessages();
+    displayTasks();
 }
 
-// Обновление видимости скрытого блока
 function updateHiddenBlock() {
     if (toDoList.length === 0) {
-        hiddenBlock.classList.remove('showBlock'); 
-        allComplBtn.classList.remove('showButton')
+        hiddenBlock.classList.remove('showBlock');
+        allComplBtn.classList.remove('showButton');
     } else {
-        hiddenBlock.classList.add('showBlock'); 
-        allComplBtn.classList.add('showButton')
+        hiddenBlock.classList.add('showBlock');
+        allComplBtn.classList.add('showButton');
     }
 }
+
 updateHiddenBlock();
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
-        todoAddTask(event, document.querySelector('.todoInputBlock__input'));
+        addTask(event, todoInput);
     }
 });
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     if (!event.target.closest('.todoInputBlock')) {
-        todoAddTask(event, document.querySelector('.todoInputBlock__input'));
+        addTask(event, todoInput);
     }
 });
 
-// Добавление задачи
-function todoAddTask(event, point){
-    if(event.key == 'Enter' || event.type === 'click'){
-        if(point.value.trim() !== '' && point.value !== null){
-            let inputValue = point.value;
-            point.value = "";
-            let newToDo ={
-                toDoTxt: inputValue,
+function addTask(event, point) {
+    if (event.key == 'Enter' || event.type === 'click') {
+        if (point.value.trim() !== '') {
+            const newToDo = {
+                toDoTxt: point.value,
                 checked: false
-            }
+            };
+            point.value = "";
             toDoList.unshift(newToDo);
-            displayMessages();
-            localStorage.setItem ("todo", JSON.stringify(toDoList));
+            displayTasks();
+            localStorage.setItem("todo", JSON.stringify(toDoList));
         }
     }
     updateHiddenBlock();
-    countLefts();
-    filteredList();   
+    updateTodoCount();
+    filterTodoList();
 }
 
-// Отображение списка задач в обратном порядке
-function displayMessages(){
-    let displayMessage = '';
-    for (let i = 0; i < toDoList.length; i++) {
-        const item = toDoList[i];
-        displayMessage += `
+function displayTasks() {
+    const displayMessage = toDoList.map((item, i) => `
         <li class='taskItem'>
             <div class='taskContent'>
-                <input type='checkbox' name='checkInput' id='item__${i}' ${item.checked ? 'checked': ''} />
+                <input type='checkbox' name='checkInput' id='item__${i}' ${item.checked && "checked"} />
                 <label id="labelTxt" data-id='${i}' class="taskContent__label ${item.checked ? 'labelChecked' : ''}">${item.toDoTxt}</label>
                 <input type="text" class="editItemInput" />
             </div>
-            <button class='taskItem__deleteBtn' data-id='${i}'></button>
+            <button class='taskItem__deleteBtn' data-id='${i}' />
         </li>
-        `;
-    }
+    `).join('');
+
     toDoItem.innerHTML = displayMessage;
 
-    let hasCheckedItem = toDoList.some(item => item.checked === true);
+    const hasCheckedItem = toDoList.some(item => item.checked === true);
+
     if (hasCheckedItem) {
-        document.querySelector('.clearComplBtn').classList.add('showBlock');
+        clearComplBtn.classList.add('showBlock');
     } else {
-        document.querySelector('.clearComplBtn').classList.remove('showBlock');
+        clearComplBtn.classList.remove('showBlock');
     }
-    addDeleteButtonEventListeners();    
+
+    addDeleteButtonEventListeners();
 }
 
-// Функция фильтрации по кнопкам
-function filteredList() {
+function filterTodoList() {
     const activeTab = document.querySelector('.tasksFilter__btn.filterActive');
-    let data_tab = activeTab.getAttribute('data-tab');
-    let taskItems = document.querySelectorAll('.taskItem');
+    const data_tab = activeTab.getAttribute('data-tab');
+    const taskItems = document.querySelectorAll('.taskItem');
 
-    taskItems.forEach(function(item) {
-        let checkbox = item.querySelector('input[type="checkbox"]');
+    taskItems.forEach(function (item) {
+        const checkbox = item.querySelector('input[type="checkbox"]');
         if (data_tab === "active" && checkbox.checked) {
             item.style.display = 'none';
         } else if (data_tab === "completed" && !checkbox.checked) {
             item.style.display = 'none';
         } else {
-            item.style.display = ''; 
+            item.style.display = '';
         }
     });
-    updateHiddenBlock(); 
+    updateHiddenBlock();
 }
 
-// Обработчик событий для кнопок удаления задачи
 function addDeleteButtonEventListeners() {
-    let deleteBtnItems = document.querySelectorAll('.taskItem__deleteBtn');
-    deleteBtnItems.forEach(function(deleteBtnItem) {
-        deleteBtnItem.addEventListener('click', function(event) {
-            let numDeleteBtn = parseInt(event.target.getAttribute('data-id'));
-            toDoList = toDoList.filter((item, index) => index !== numDeleteBtn);
+    const deleteBtnItems = document.querySelectorAll('.taskItem__deleteBtn');
+    deleteBtnItems.forEach(function (deleteBtnItem) {
+        deleteBtnItem.addEventListener('click', function (event) {
+            const numDeleteBtn = parseInt(event.target.getAttribute('data-id'));
+            toDoList.splice(numDeleteBtn, 1); 
             localStorage.setItem("todo", JSON.stringify(toDoList));
             updateHiddenBlock();
-            displayMessages();
-            countLefts();
-            filteredList();
+            displayTasks();
+            updateTodoCount();
+            filterTodoList();
         });
     });
 }
- 
-// Обработчик изменения состояния чекбоксов
-toDoItem.addEventListener('change', function(event) {
+
+toDoItem.addEventListener('change', function (event) {
     if (event.target.type === 'checkbox') {
-        let idInput = event.target.getAttribute('id');
-        let labelFor = document.querySelector(`[data-id="${idInput}"]`);
+        const idInput = event.target.getAttribute('id');
+        const labelFor = document.querySelector(`[data-id="${idInput}"]`);
 
         if (labelFor) {
             if (event.target.checked) {
@@ -131,17 +131,15 @@ toDoItem.addEventListener('change', function(event) {
                 labelFor.classList.remove('labelChecked');
             }
         }
-
-        let itemIndex = parseInt(idInput.split('__')[1]);
+        const itemIndex = parseInt(idInput.split('__')[1]);
         toDoList[itemIndex].checked = event.target.checked;
         localStorage.setItem("todo", JSON.stringify(toDoList));
-        displayMessages();
-        countLefts();
-        filteredList();
+        displayTasks();
+        updateTodoCount();
+        filterTodoList();
     }
 });
 
-// Функция для установки ширины 
 function updateEditItemInputWidth() {
     const tasks = document.querySelectorAll('.taskItem');
     tasks.forEach(tasks => {
@@ -151,57 +149,55 @@ function updateEditItemInputWidth() {
     });
 }
 
-// Обработчик двойного клика для редактирования & редактирование
-toDoItem.addEventListener('dblclick', function(event) {
-    if (event.target.tagName === 'LABEL') { 
-        let labelId = event.target.getAttribute('data-id');
-        let labelTxt = event.target.innerText;
-        let input = event.target.nextElementSibling;     
+toDoItem.addEventListener('dblclick', function (event) {
+    if (event.target.tagName === 'LABEL') {
+        const labelId = event.target.getAttribute('data-id');
+        const labelTxt = event.target.innerText;
+        const input = event.target.nextElementSibling;
         input.classList.add('showBlock');
-        input.value = labelTxt;   
+        input.value = labelTxt;
         input.select();
         updateEditItemInputWidth();
-        function saveChanges() {
-            if (input.value.trim() === '') { 
-                toDoList.splice(labelId, 1);
-                localStorage.setItem("todo", JSON.stringify(toDoList)); 
-                updateHiddenBlock();
-                displayMessages(); 
-                countLefts(); 
-                filteredList();
-            } else {
-                event.target.previousElementSibling.innerText = input.value;
-                input.classList.remove('showBlock');
-                toDoList[labelId].toDoTxt = input.value;  
-                localStorage.setItem("todo", JSON.stringify(toDoList));  
-                displayMessages(); 
-                countLefts();     
-                filteredList();          
-            }
-        }
-        
-        input.addEventListener('keyup', function(event) {
+
+        input.addEventListener('keyup', function (event) {
             if (event.key === 'Enter') {
-                saveChanges();
+                saveChanges(labelId, input);
             } else if (event.key === 'Escape') {
                 input.classList.remove('showBlock');
-                input.value = labelTxt; 
+                input.value = labelTxt;
             }
         });
-        
-        document.addEventListener('click', function(event) {
-            let isInputClicked = input.contains(event.target) || event.target === input;
-            let isInputActive = input.classList.contains('showBlock');
+
+        document.addEventListener('click', function (event) {
+            const isInputClicked = input.contains(event.target) || event.target === input;
+            const isInputActive = input.classList.contains('showBlock');
             if (!isInputClicked && isInputActive) {
-                saveChanges();
+                saveChanges(labelId, input);
             }
-        });
-    } 
+        }, { once: true });
+    }
 });
 
-var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-// Получение информации об отмеченных чекбоксах
-function notAllCheckboxesChecked() {
+function saveChanges(labelId, input) {
+    if (input.value.trim() === '') {
+        toDoList.splice(labelId, 1);
+        localStorage.setItem("todo", JSON.stringify(toDoList));
+        updateHiddenBlock();
+        displayTasks();
+        updateTodoCount();
+        filterTodoList();
+    } else {
+        input.previousElementSibling.innerText = input.value;
+        input.classList.remove('showBlock');
+        toDoList[labelId].toDoTxt = input.value;
+        localStorage.setItem("todo", JSON.stringify(toDoList));
+        displayTasks();
+        updateTodoCount();
+        filterTodoList();
+    }
+}
+
+function detectUncheckedCheckboxes() {
     let allChecked = true;
     checkboxes.forEach((checkbox) => {
         if (!checkbox.checked) {
@@ -212,89 +208,74 @@ function notAllCheckboxesChecked() {
     return !allChecked;
 }
 
-// Кнопка отметить все задачи/снять все задачи
 function selectAllTasks() {
-    if (notAllCheckboxesChecked()) {
-        checkboxes.forEach(function(checkbox) {
-            checkbox.checked = true;  
+    if (detectUncheckedCheckboxes()) {
+        checkboxes.forEach(function (checkbox) {
+            checkbox.checked = true;
         });
-        toDoList.forEach(function(item) {
+        toDoList.forEach(function (item) {
             if (item.checked !== true) {
-                item.checked = true;       
-                displayMessages();   
+                item.checked = true;
+                displayTasks();
                 localStorage.setItem("todo", JSON.stringify(toDoList));
             }
         });
     } else {
-        checkboxes.forEach(function(checkbox) {
-            checkbox.checked = false;    
+        checkboxes.forEach(function (checkbox) {
+            checkbox.checked = false;
         });
-        toDoList.forEach(function(item) {
-            if (item.checked === true) { 
-                item.checked = false;   
-                displayMessages();       
+        toDoList.forEach(function (item) {
+            if (item.checked === true) {
+                item.checked = false;
+                displayTasks();
                 localStorage.setItem("todo", JSON.stringify(toDoList));
             }
         });
     }
-    countLefts();
-    filteredList();
+    updateTodoCount();
+    filterTodoList();
 }
 
-// Удалить выполненные задачи
 function clearCompleted() {
-    let filteredTasks  = toDoList.filter(function(item) {
+    const filteredTasks = toDoList.filter(function (item) {
         return !item.checked;
     });
-    toDoList = filteredTasks;
+    toDoList.length = 0;
+    filteredTasks.forEach(task => {
+        toDoList.push(task);
+    });
+
     localStorage.setItem("todo", JSON.stringify(toDoList));
-    displayMessages();
+    displayTasks();
     updateHiddenBlock();
-    filteredList();
+    filterTodoList();
 }
 
-let countLeft;
-let spanLeft = document.querySelector('.tasksCount');
-
-// Подсчет оставшихся задач
-function countLefts(){
-    countLeft = 0; 
-    toDoList.forEach(function(item){
-        if(item.checked === false){
+function updateTodoCount() {
+    countLeft = 0;
+    toDoList.forEach(function (item) {
+        if (item.checked === false) {
             countLeft++;
         }
     });
     spanLeft.innerHTML = countLeft;
-    if (countLeft === 0){
-        let style = document.createElement('style');
-        style.innerHTML = `
-        .allCompl__label::before {
-            color: #484848;
-        }
-        `;
-        document.head.appendChild(style);
+
+    if (countLeft === 0) {
+        btnAllCheck.classList.add('black');
     } else {
-        let style = document.createElement('style');
-        style.innerHTML = `
-        .allCompl__label::before {
-            color: #949494;
-        }
-        `;
-        document.head.appendChild(style);
+        btnAllCheck.classList.remove('black');
     }
 }
 
-// Обработчик клика по вкладкам-фильтрам
-tabsBtn.forEach(function(item) {
-    item.addEventListener("click", function() {
-        let currentBtn = item;
-        let tabId = currentBtn.getAttribute('data-tab');
-        tabsBtn.forEach(function(item) {
+tabsBtn.forEach(function (item) {
+    item.addEventListener("click", function () {
+        const currentBtn = item;
+        tabsBtn.forEach(function (item) {
             item.classList.remove('filterActive');
         });
         currentBtn.classList.add('filterActive');
-        filteredList();
+        filterTodoList();
     });
 });
 
-countLefts();
+updateTodoCount();
